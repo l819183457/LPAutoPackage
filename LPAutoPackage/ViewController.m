6307772dec323635e7fe4ad8d58e9e35eeb65fda
ziqiang.liang <ziqiang.liang@ehking.com>
2018-08-31 18:39:12 +0800

    自动化打版本

--- LPAutoPackage/ViewController.m
+++ LPAutoPackage/ViewController.m
@@ -7,7 +7,8 @@
 //
 
 #import "ViewController.h"
-
+//#define infoPath   [[NSBundle mainBundle] pathForResource:@"LPAutoPackage.plist" ofType:nil]
+//#define  infoChannel [[NSDictionary dictionaryWithContentsOfFile:infoPath] objectForKey:@"CURRENT_CONFIGURATION"]
 @interface ViewController ()
 
 @end
@@ -16,6 +17,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSString * str =  [[NSBundle mainBundle] pathForResource:@"Info.plist" ofType:nil];
+    
+    NSString * dic =    [[NSDictionary dictionaryWithContentsOfFile:str] objectForKey:@"CURRENT_CONFIGURATION"];
+    
+    
+    UIAlertView * alert = [[UIAlertView alloc]initWithTitle:nil message:dic delegate:self cancelButtonTitle:@"123" otherButtonTitles: nil];
+    [alert show];
+    
+    
+    
+//    dic objectForKey:@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
